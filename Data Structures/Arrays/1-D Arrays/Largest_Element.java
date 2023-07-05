3dc644bc3aa4a0d8e6b6c158fc595db3a503dfc6
Abhishek Sharma <108231392+abhishekS100@users.noreply.github.com>
2023-07-05 13:23:36 +0530

    Initial Commit

---
+++ Data Structures/Arrays/1-D Arrays/Largest_Element.java
@@ -0,0 +1,27 @@
+
+
+import java.util.*;
+
+class Test{
+
+  static int printArray(int[] arr){
+    
+    int n = arr.length;
+    int max = Integer.MIN_VALUE;
+    
+    for(int i = 0; i < n; i++){
+        if(arr[i] > max){
+          max = arr[i];  
+        }
+    }
+    return max;
+  }
+  
+  public static void main(String[] args){
+    
+     int[] arr = {5,8,2,9,3,4,1};
+    
+    System.out.println(printArray(arr));
+    
+  }
+}
