dc355729596656f1e9b0bb98d025c8438081078b
Abhishek Sharma <108231392+abhishekS100@users.noreply.github.com>
2023-07-05 13:55:13 +0530

    Inital Commit

---
+++ Data Structures/Arrays/1-D Arrays/Is_Sorted.java
@@ -0,0 +1,27 @@
+
+import java.util.*;
+class Test{
+
+  static boolean isSorted(int[] arr){
+    
+    int n = arr.length;
+    boolean check = true;
+    
+    for(int i = 1; i < n; i++){
+       
+       if(arr[i] < arr[i-1]){
+          check = false;
+          break;
+        }
+    }
+    return check;
+   }
+  
+  public static void main(String[] args){
+    
+     int[] arr = {1,2,3,5,6,7,8};
+    
+     System.out.println(isSorted(arr));
+    
+  }
+}
