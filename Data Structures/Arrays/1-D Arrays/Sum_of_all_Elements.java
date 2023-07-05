1aadc4d12963f63fbf53f79bd0952ac05a2af148
Abhishek Sharma <108231392+abhishekS100@users.noreply.github.com>
2023-07-05 13:01:27 +0530

    Create Sum_of_all_Elements.java

---
+++ Data Structures/Arrays/1-D Arrays/Sum_of_all_Elements.java
@@ -0,0 +1,21 @@
+
+class Test{
+    
+    static int sum(int[] arr){
+        
+        int sum = 0;
+        
+        for(int i = 0; i < arr.length;i++){
+            sum += arr[i];
+        }
+        
+        return sum;
+    }
+  
+    public static void main(String[] args){
+      
+        int[] arr = {1,5,3,7,8,2};
+      
+        System.out.println(sum(arr));
+    }
+}
