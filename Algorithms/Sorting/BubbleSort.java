a9bfede41017ebda3f8a15ee5ce210bf519822cf
Abhishek Sharma <108231392+abhishekS100@users.noreply.github.com>
2023-09-29 09:55:59 +0530

    Update

--- Algorithms/Sorting/BubbleSort.java
+++ Algorithms/Sorting/BubbleSort.java
@@ -15,13 +15,18 @@ class BubbleSort {
     public static void bubbleSort(int[] arr){
         int n = arr.length;
         for(int i = 0; i < n-1;i++){
+            boolean flag = false;
             for(int j = 0; j < n-1-i; j++){
                if( arr[j] > arr[j+1]){
                 int temp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = temp;
+                flag = true; 
                } 
             }
+         if(!flag){
+            return;
+          }
         }
     }
     public static void printArray(int[] arr){
